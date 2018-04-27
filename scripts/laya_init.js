4abd82fabe318bd489633498f5fd758d13b86c7d
blockly-jump <38748230+blockly-jump@users.noreply.github.com>
2018-04-27 17:47:03 +0800

    Update laya_init.js

--- scripts/laya_init.js
+++ scripts/laya_init.js
@@ -1,14 +1,14 @@
 
 function laya_init(){
-Laya.init(800, 600); 
+Laya.init(600, 400); 
 Laya.stage.bgColor = "#232628"
 Laya.stage.scaleMode = "fixedauto";
 
 var canvas = document.getElementById("layaCanvas");
 canvas.style.left = '60px';
 canvas.style.top = '90px';
-canvas.style.width = '800px';
-canvas.style.height = '600px';
+canvas.style.width = '600px';
+canvas.style.height = '400px';
 }
 
 
